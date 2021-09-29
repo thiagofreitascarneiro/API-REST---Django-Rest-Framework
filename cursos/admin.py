b415033ad045c07df54a86d132050122acbc98df
thiago <thiagofreitascarneiro@hotmail.com>
2021-09-29 16:55:06 -0300

    atualizando o admin

--- cursos/admin.py
+++ cursos/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Curso, Avaliacao
 
+
 @admin.register(Curso)
 class CursoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'url', 'ciracao', 'atualizacao', 'ativo')
